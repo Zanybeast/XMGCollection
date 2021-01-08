216405dac2dc0bda44d0ac8c2c752661ecd93424
Carl <zeng1090684394@163.com>
2021-01-08 13:08:37 +0800

    Local Initial

---
+++ Lottery/Lottery/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Lottery
+//
+//  Created by carl on 2021/1/8.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Lottery/Lottery/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Lottery
+//
+//  Created by carl on 2021/1/8.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
